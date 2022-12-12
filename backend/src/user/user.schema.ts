9df4c3c2de8b996d770bbb0b5c7d1abc9ee30854
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-13 02:47:23 +0900

    feat : userSchema

---
+++ backend/src/user/user.schema.ts
@@ -0,0 +1,30 @@
+import { Schema } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    role: { // TODO default와 enum 충돌 안나는지 확인하기
+      type: String,
+      enum: ['USER', 'ADMIN'],
+      required: false,
+      default: 'USER'
+    }
+  },
+  {
+    collection: 'users',
+    timestamps: true
+  }
+);
+
+export { UserSchema };
