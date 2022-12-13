fec9f52170c2b269e7b7f6eae5f631c6fca80981
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-13 23:21:46 +0900

    fix : UserSchema replace email & password with auth

--- backend/src/user/user.schema.ts
+++ backend/src/user/user.schema.ts
@@ -1,20 +1,21 @@
 import { Schema } from 'mongoose';
 
-const UserSchema = new Schema<User>(
+// timestamp => createdAt, updatedAt
+// Auth => 비밀번호 변경
+// User => 사용자 정보 변경
+
+const UserSchema = new Schema<UserT>(
   {
-    email: {
-      type: String,
-      required: true
-    },
-    password: {
-      type: String,
-      required: true
+    auth: {
+      type: Schema.Types.ObjectId,
+      ref: 'auths'
     },
     username: {
       type: String,
       required: true
     },
-    role: { // TODO default와 enum 충돌 안나는지 확인하기
+    role: {
+      // TODO default와 enum 충돌 안나는지 확인하기
       type: String,
       enum: ['USER', 'ADMIN'],
       required: false,
