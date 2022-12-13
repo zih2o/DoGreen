58405a59928d7ff10e965ddbcecb7fa19e0254dd
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-13 23:18:01 +0900

    feat : UserDTO, userInterface

--- backend/src/user/user.d.ts
+++ backend/src/user/user.d.ts
@@ -1,18 +1,21 @@
 // union type
 type Role = 'USER' | 'ADMIN'
 // DTO
-type User = {
-    email: string,
-    password: string,
+type UserT = {
+    auth: AuthT,
     username: string,
     role: Role
 };
+
+type CreateUserDto = {
+    username: string,
+    email: string,
+}
 interface IUserService {
-    RegisterUser: (newUser: User)=> Promise<void>;
-    findAll: ()=> Promise<User[]>;
-    findUser: ()=> Promise<User | null> ;
+    findAll: ()=> Promise<UserT[]>;
+    findUser: ()=> Promise<UserT | null> ;
     // DTO에선 없지만 controller의 미들웨어 같은 걸로 email을 넣어주어야 함
-    updateUser: (userInfo: Omit<User, 'role'>)=> Promise<void>;
-    deleteUser: ()=> Promise<void>;
-    findUserByEmail: (email: string)=> Promise<User | null>;
+    updateUser: (userInfo: Omit<UserT, 'role'>) => Promise<void>;
+    deleteUser: (email: UserT['email'])=> Promise<void>;
+    createUser: (userInfo: CreateUserDto) => Promise<void>;
 }
