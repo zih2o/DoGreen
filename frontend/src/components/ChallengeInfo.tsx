6214541d7ddeb845403b01e0b177198c41350d23
Yena Lee <lmonari71@gmail.com>
2022-12-14 18:55:51 +0000

    feat : 마이페이지 환경 구축

---
+++ frontend/src/components/ChallengeInfo.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import CalendarHeatmap from 'react-calendar-heatmap';
+// import 'react-calendar-heatmap/dist/styles.css';
+import './MyPage.css';
+
+const ChallengeInfo = () => {
+  return (
+    <div>
+      <h2 className="text-3xl pl-20 mb-10 font-bold">참여하고 있는 챌린지</h2>
+      <div className="challenge_wrapper flex items-center justify-around h-64 mx-20 bg-green-50">
+        <h2 className="text-3xl text-center font-bold">플라스틱 줄이기 챌린지</h2>
+        <h2 className="text-2xl  font-bold">
+          참여율 <br></br>55%
+        </h2>
+        {/* py-2 px-4 pt-8 */}
+        <div className="show_status flex items-center mt-8 w-96 ">
+          <CalendarHeatmap
+            startDate={new Date('2022-09-01')}
+            endDate={new Date('2022-12-31')}
+            values={[
+              { date: '2022-09-10', count: 4 },
+              { date: '2022-10-22', count: 2 },
+              { date: '2022-12-01', count: 3 },
+              { date: '2022-12-02', count: 1 },
+              { date: '2022-12-03', count: 2 },
+              { date: '2022-11-01', count: 2 },
+              { date: '2022-11-21', count: 1 },
+              { date: '2022-10-11', count: 4 },
+              { date: '2022-10-12', count: 3 },
+            ]}
+            showWeekdayLabels={true}
+            classForValue={(value) => {
+              if (!value) {
+                return 'color-empty';
+              }
+              return `color-github-${value.count}`;
+            }}
+            // titleForValue={(value) => {
+            //   console.log(value);
+            //   if (!value) {
+            //     return;
+            //   } else {
+            //     console.log(`Date is ${value.date}`);
+            //   }
+            // }}
+            // onMouseOver={(value) => {
+            //   if (!value) {
+            //     return;
+            //   } else {
+            //     console.log(`Date is ${value.date}`);
+            //   }
+            // }}
+            tooltipDataAttrs={(value: any) => {
+              const d1 = new Date(value.date);
+              if (value) {
+                return {
+                  'data-tip': `${d1.toISOString().slice(0, 10)} has count: ${value.count}`,
+                };
+              }
+            }}
+            onClick={(value) => {
+              if (value) alert(`참여하신 날짜는 ${value.date} 입니다`);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ChallengeInfo;
---
+++ frontend/src/pages/MyPage.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import UserInfo from '../components/UserInfo';
+import MyPageTab from '../components/MyPageTab';
+// import axios from 'axios';
+
+export interface userData {
+  id: number;
+  name: string;
+  rank: number;
+  status: string;
+  saveNumber: number;
+  userAnimal: string;
+}
+export const InitialData: userData = {
+  id: 0,
+  name: '엘리스',
+  rank: 60,
+  status: 'Earth Gardian',
+  saveNumber: 50,
+  userAnimal: '아델리펭귄',
+};
+
+const MyPage = () => {
+  return (
+    <div className="w-full min-h-screen">
+      <div className="bg-rose-100 py-24">
+        <h2 className="text-3xl font-bold underline">마이페이지</h2>
+      </div>
+      <div className="flex min-h-screen w-full">
+        <div className="MyPage_Tab_Wrapper w-1/4 bg-gray-200">
+          <MyPageTab />
+        </div>
+        <div className="MyPage_Content_Wrapper w-3/4 bg-slate-100">
+          <UserInfo />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MyPage;
