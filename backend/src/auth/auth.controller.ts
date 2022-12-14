3acbf2c19b75eb0bd5186bb3c4a4d5d4bdaad4e6
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-14 12:27:40 +0900

    feat : login, register

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,47 +1,46 @@
 import express, { Request, Response } from 'express';
 import { UserService } from '../user/user.service';
 
-const router = express.Router();
-
 const authService = {} as IAuthService;
 const userService = new UserService();
 
-export class AuthController { 
-  async register(req: Request, res: Response){
-    const { username, email, password } = req.body; // username, email, password
-    await authService.register({ email, password });
+export class AuthController {
+  async register(req: Request, res: Response) {
+    const {
+      username,
+      email,
+      password,
+      role
+    } = req.body; // username, email, password
+    await authService.register({ email, password, role });
     await userService.createUser({ username, email });
     res.status(201);
   };
-  router.post('/login', async (req: Request, res: Response) => {
+
+  //  _id를 어떻게꺼내지?
+  // login
+  /*
+  user email과 password를 받는다
+  email로 auth 정보 찾아온다
+  auth의 password와 db의 password를 argon2로 compare한다
+
+  일치하면 jwt로 auth 정보를 암호화해서 json web token 문자열로 만든다
+  사용자에게 준다
+  */
+  async login(req: Request, res: Response) {
     const { email, password } = req.body;
-    // const { username, password } = req.body;
-  
-    // Filter user from the users array by username and password
-  
     const userToken = await userService.getUserToken({ email, password });
-    res.json(userToken);
-  });
+    // post요청인데 과연 200이 맞을까
+    res.status(200).json(userToken);
+  };
   // SOFT DELTE 탈퇴
-  router.patch('/signout', (req: Request, res: Response) => {});
-  
   // register
   /*
   user 정보를 받는다
   Auth 생성 (password는 argon2로 암호화)
   user 생성
   */
-  
-  // login
-  /*
-  user email과 password를 받는다
-  email로 auth 정보 찾아온다
-  auth의 password와 db의 password를 argon2로 compare한다
-  
-  일치하면 jwt로 auth 정보를 암호화해서 json web token 문자열로 만든다
-  사용자에게 준다
-  */
-  
+
   // login required
   /*
   클라이언트는 Authorization 헤더에 token을 보낸다
@@ -51,5 +50,4 @@ export class AuthController {
   -> 잘못된 토큰이면 에러!
   -> 올바른 토큰이면 next 허가
   */
-  
 };
