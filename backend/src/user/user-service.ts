902321dfde59caf99e9caf26df2b35631a76e70f
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-16 15:17:22 +0900

    form : 주석 제거

--- backend/src/user/user-service.ts
+++ backend/src/user/user-service.ts
@@ -4,14 +4,6 @@ import invariant from '../invariant';
 import { UserSchema } from './user-schema';
 
 const UserModel = model<UserT>('users', UserSchema);
-// ㅇㅇ findAll: ()=> Promise<UserDto[]>; // admin
-// ㅇㅇ findUser: (authId: string)=> Promise<UserDto | null>; // user가 자신의 정보를 조회함
-// // DTO에선 없지만 controller의 미들웨어 같은 걸로 email을 넣어주어야 함
-// ㅇㅇ updateUser: (userInfo: Omit<UserT, 'role'>) => Promise<void>; // user가 자신의 정보를 수정하는 기능
-// ddㅇㅇdeleteUser: (email: UserT['email'])=> Promise<void>; // 유저가 스스로 탈퇴할 수 있는 기능
-// ㅇㅇ createUser: (userInfo: CreateUserDto) => Promise<void>; // register
-// ㅇㅇfindUserByUsername: (userEmail: UserT['email']) => Promise<UserDto>;
-// ㅇㅇ existUserByEmail: (userEmail: UserT['email'])=> Promise<boolean>;
 
 export class UserService implements IUserService {
   async findAll() {
