5898927ec3488b63a024c6bd6628421a97a1985e
Yena Lee <lmonari71@gmail.com>
2022-12-17 02:20:06 +0900

    feat : 마이페이지 tailwind css 적용

--- frontend/src/components/Progressbar.tsx
+++ frontend/src/components/Progressbar.tsx
@@ -9,13 +9,16 @@ const TotalProgressBar = ({ value }: progressInfo) => {
     <div className="progressbar_wrapper mt-12 mb-24 px-10 lg:px-20 mx-auto">
       <div className="progress-bar">
         <div className="value">
-          <div style={{ color: 'green', width: value + 3 + '%' }}>
+          <div className="garden1" style={{ width: value + 3 + '%' }}>
             <img className="w-20 h-20 float-right text-center" src="/src/assets/profile.png" alt="profile" />
           </div>
         </div>
         <div className="bar">
-          <div style={{ backgroundColor: 'green', width: value + '%' }}></div>
-          <div style={{ backgroundColor: '#d3d3d3', width: 100 - value + '%' }}></div>
+          <div className="inline-block h-8 rounded-tl-md rounded-bl-md bg-garden1" style={{ width: value + '%' }}></div>
+          <div
+            className="inline-block h-8 rounded-tr-md rounded-br-md bg-slate-100"
+            style={{ width: 100 - value + '%' }}
+          ></div>
         </div>
       </div>
     </div>
