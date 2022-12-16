150da70f4cc2e35a318f66af56b7a9fae39c9bd3
Jisu Park <pj2sharon55@gmail.com>
2022-12-16 02:40:38 +0000

    221216-FE-뉴스페이지

---
+++ frontend/src/components/CardPageNav.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CardPageNav() {
+  return <div>카드페이지네브</div>;
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import Admin from './components/Admin';
+import Home from './components/Home';
+import './index.css';
+import CardPage from './pages/CardPage';
+import MyPage from './pages/MyPage';
+import NewsPage from './pages/NewsPage';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/mypage', element: <MyPage /> },
+      { path: '/admin', element: <Admin /> },
+      { path: '/categories/:catId', element: <CardPage /> },
+      { path: '/cards/:cardId', element: <NewsPage /> },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
