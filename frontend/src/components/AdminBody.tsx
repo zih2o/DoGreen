8a6a59a8ba420c69d8cb8e3eca07bbbc00031137
SIYEA J <siyea0405@gmail.com>
2022-12-19 07:10:21 +0900

    feat: Admin page CSS

--- frontend/src/components/AdminBody.tsx
+++ frontend/src/components/AdminBody.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Card from './Card';
 function AdminBody(props) {
   return (
     <>
       <div className="grid col-span-3 row-span-full grid-cols-4 grid-rows-6 lg:col-span-2">
         <div className="flex flex-wrap justify-between col-span-full p-6 pb-8 bg-garden1 text-garden2 ">
-          <img className="max-w-[50%] m-1" src="/src/assets/profile.png" alt="profile_img" />
+          <img className="m-1" src="/src/assets/profile.png" alt="profile_img" />
           <ul className="flex flex-col justify-center">
-            <li className='text-l mb-2 md:text-xl font-semibold'>관리자</li>
-            <li className='text-l md:text-xl font-semibold'>Lv.Admin</li>
+            <li className="text-l mb-2 md:text-xl font-semibold">관리자</li>
+            <li className="text-l md:text-xl font-semibold">Lv.Admin</li>
           </ul>
         </div>
         <div className="col-span-full row-start-2 row-span-full p-6 bg-garden1 text-garden2 ">
           <ul className="flex flex-col">
-            <li className='text-l mt-5 md:text-xl font-bold'>동물카드 관리</li>
-            <li className='text-l mt-5 md:text-xl font-bold'>뉴스레터카드 관리</li>
-            <li className='text-l mt-5 md:text-xl font-bold'>챌린지 게시글/리뷰 관리</li>
+            <Link className="hover:underline" to="#">
+              <li className="text-l mt-5 md:text-xl font-bold">동물카드 관리</li>
+            </Link>
+            <Link className="hover:underline" to="#">
+              <li className="text-l mt-5 md:text-xl font-bold">뉴스레터카드 관리</li>
+            </Link>
+            <Link className="hover:underline" to="#">
+              <li className="text-l mt-5 md:text-xl font-bold">챌린지 게시글/리뷰 관리</li>
+            </Link>
           </ul>
         </div>
       </div>
-      <div className='col-start-4 col-span-full row-span-full bg-garden2 lg:col-start-3'>내용</div>
+      <div className="flex flex-col p-6 col-start-4 col-span-full row-span-full bg-garden2 lg:col-start-3">
+        <div className="flex flex-row justify-between p-4 item-bottom">
+          <h2 className="flex text-4xl font-bold mb-5">동물카드 관리_첫번째 카드는 생성하기 카드로만들예정</h2>
+        </div>
+        <div className="cardSapce flex flex-wrap flex-row">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </>
   );
 }
--- frontend/src/components/Hamburger.tsx
+++ frontend/src/components/Hamburger.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 function Hamburger(props) {
+  const [onUser, setOnUser] = useState(false);
   return (
     <>
       <div className="p-6 fixed top-[12.3%] bottom-[100px] right-[0%] w-[70%] h-full items-center place-content-between bg-garden4 text-right border-b-2 border-garden4 md:w-[30%]">
-        <div className="flex flex-col mx-auto items-center">
-          <div className="leading-10 text-garden2 text-3xl">
-            {props.rank} {props.name}님
+        {onUser ? (
+          <div className="flex flex-col mx-auto items-center">
+            <div className="leading-10 text-garden2 text-3xl">
+              {props.rank} {props.name}님
+            </div>
+            <span className=" text-garden2 text-xl">지금까지 펭귄 300마리를 구하셨어요!</span>
           </div>
-          <span className=" text-garden2 text-xl">지금까지 펭귄 300마리를 구하셨어요!</span>
-        </div>
+        ) : (
+          <div className="flex flex-col">
+            <label htmlFor="id">
+              <input type="text" placeholder="ID" className="p-1" />
+            </label>
+            <label htmlFor="password" className="mt-2">
+              <input type="password" placeholder="Password" className="p-1" />
+            </label>
+          </div>
+        )}
         <ul id="menu" className="flex flex-col mt-6">
           <li className="mb-4 text-garden2">
             <Link to="/" className="hover:underline decoration-garden2">
