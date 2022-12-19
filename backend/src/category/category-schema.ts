1ded33620ae807597e74b71b4dcb7be60443f337
Son HyeongSeok <74246053+sonhs1996@users.noreply.github.com>
2022-12-19 20:45:50 +0900

    fix : category schema override

--- backend/src/category/category-schema.ts
+++ backend/src/category/category-schema.ts
@@ -1,19 +1,26 @@
 import { Schema } from 'mongoose';
 
 const CategorySchema = new Schema({
-  name: {
+  categoryName: {
     type: String,
-    require: true,
-    unique: true
+    required: true,
+    unique: true,
+    maxlength: 15
   },
-  index: {
-    type: Number,
-    require: true,
-    unique: true
+  MascotName: {
+    type: String,
+    required: true,
+    unique: true,
+    maxlength: 10
   },
-  cards: [{
+  MascotImage: {
+    type: String,
+    required: true,
+    default: 'https://user-images.githubusercontent.com/91370858/208048148-47028f2f-d283-4ab1-a43e-3c073543161e.png'
+  },
+  posts: [{
     type: Schema.Types.ObjectId,
-    ref: 'Card'
+    ref: 'posts'
   }]
 });
 
