344ea924bf7727ec54db65540a4f4a8cbfa44645
Son HyeongSeok <74246053+sonhs1996@users.noreply.github.com>
2022-12-19 20:55:06 +0900

    fix : field rename to camel case

--- backend/src/category/categorySchema.ts
+++ backend/src/category/categorySchema.ts
@@ -7,13 +7,13 @@ const CategorySchema = new Schema({
     unique: true,
     maxlength: 15
   },
-  MascotName: {
+  mascotName: {
     type: String,
     required: true,
     unique: true,
     maxlength: 10
   },
-  MascotImage: {
+  mascotImage: {
     type: String,
     required: true,
     default: 'https://user-images.githubusercontent.com/91370858/208048148-47028f2f-d283-4ab1-a43e-3c073543161e.png'
