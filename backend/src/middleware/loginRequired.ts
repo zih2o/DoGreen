d185982d2c85b4bee9ee91bd8eda1dc51acfb721
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-20 16:31:10 +0900

    refactor : verify currentUser를 authService로 추출

--- backend/src/middleware/loginRequired.ts
+++ backend/src/middleware/loginRequired.ts
@@ -1,43 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { AuthService } from '../auth/auth.service';
+import { ForbiddenError } from '../errors/ForbiddenError';
 import invariant from '../invariant';
-import logger from '../logger';
+
+const authService = new AuthService();
 
 const loginRequired = (req: Request, res: Response, next: NextFunction) => {
   const userToken = req.headers['authorization']?.split(' ')[1];
 
-  if (!userToken || userToken === 'null') {
-    res.status(403).json({
-      result: 'forbidden-approach',
-      reason: '로그인한 유저만 사용할 수 있는 서비스입니다.'
-    });
+  invariant(
+    userToken !== undefined && userToken !== '',
+    new ForbiddenError('로그인해야 이용할 수 있는 서비스입니다.')
+  );
 
-    return;
-  }
+  const currentUser = authService.verifyCurrentUser(userToken);
 
-  // 해당 token 이 정상적인 token인지 확인
-  try {
-    const secretKey = process.env.JWT_SECRET || 'secret-key';
-    const currentUser = jwt.verify(userToken, secretKey);
-    invariant(typeof currentUser === 'object', 'jwt payload는 객체여야 합니다');
-
-    const context = {
-      currentUser: currentUser as {
-        authId: string,
-        email: string,
-        role: Role,
-      }
-    };
-
-    req.context = context;
-  } catch (error) {
-    logger.error(error);
-    res.status(403).json({
-      result: 'forbidden-approach',
-      reason: '정상적인 토큰이 아닙니다.'
-    });
-    return;
-  }
+  req.context = { currentUser };
 
   next();
 };
