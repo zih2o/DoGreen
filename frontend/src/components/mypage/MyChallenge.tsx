47191dbf47e03084ed55b9d87d8ee6d6643317fd
Yena Lee <lmonari71@gmail.com>
2022-12-20 12:44:06 +0900

    refactor : 폴더 상,하위 구조 변경
    common 폴더 > theme.tsx
    layout 폴더 > GlobalLayout.tsx , MyPageLayout.tsx
    mypage 폴더 > 마이페이지 내 세부페이지 컴포넌트

---
+++ frontend/src/components/mypage/MyChallenge.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import MyPageTopBar from './MyPageTopBar';
+import MyPageTab from './MyPageTab';
+import ChallengeInfo from './ChallengeInfo';
+import { GlobalLayout } from '../layout/GlobalLayout';
+import { MyPageLayout } from '../layout/MyPageLayout';
+
+const MyChallenge = () => {
+  return (
+    <GlobalLayout>
+      <MyPageTopBar />
+      <MyPageLayout>
+        <MyPageTab />
+        <ChallengeInfo />
+      </MyPageLayout>
+    </GlobalLayout>
+  );
+};
+export default MyChallenge;
