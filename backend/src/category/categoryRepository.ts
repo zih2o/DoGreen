bd6fa91703ba0c59b649f2a08f9b5929f7fb91cc
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-22 00:58:20 +0900

    feat : categoryId들로 카테고리 조회하는 기능 추가
    fix typo

--- backend/src/category/categoryRepository.ts
+++ backend/src/category/categoryRepository.ts
@@ -1,7 +1,9 @@
 import { model, Types } from 'mongoose';
+import { BadRequestError } from '../errors/BadRequestError';
+import invariant from '../invariant';
 import { CategorySchema } from './categorySchema';
 
-const CategoryModel = model<categoryT>('cateogories', CategorySchema);
+const CategoryModel = model<categoryT>('categories', CategorySchema);
 
 export class CategoryRepository implements ICategoryRepository {
   async updateOneCategory(toUpdate:updateCategoryDto, id:updateCategoryDto['id']) {
@@ -10,6 +12,11 @@ export class CategoryRepository implements ICategoryRepository {
 
   async deleteOneCategory(id: categoryT['id']) {
     await CategoryModel.deleteOne(id);
+  }
+
+  async findCategoryByIds(ids: categoryT['id'][]) {
+    invariant(ids !== null, new BadRequestError('id에 해당하는 카테고리가 존재하지 않습니다.'));
+    return CategoryModel.find({ _id: ids });
   }
 
   async findAllCategory() {
