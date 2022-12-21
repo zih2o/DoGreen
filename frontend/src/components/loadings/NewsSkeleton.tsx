4523d2935001ce2e75e764fa461a065d7f375a67
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2022-12-21 11:15:43 +0900

    feat: 뉴스페이지 코멘트모달 CSS

--- frontend/src/components/loadings/NewsSkeleton.tsx
+++ frontend/src/components/loadings/NewsSkeleton.tsx
@@ -6,7 +6,7 @@ export default function NewsSkeleton() {
       <div className="flex flex-col items-center mr-4">
         <div className={'w-12 h-12 rounded-full bg-zinc-300 shadow-xl animate-pulse'} />
       </div>
-      <div className="flex flex-col w-10/12 rounded-lg shadow-2xl  md:w-9/12 lg:w-8/12">
+      <div className="flex flex-col z-0 w-10/12 rounded-lg shadow-2xl  md:w-9/12 lg:w-8/12">
         <div className="w-full p-6 rounded-t-lg bg-slate-50 text-md sm:">
           <div className="w-10/12 h-3 rounded-sm bg-zinc-300 animate-pulse" />
           <div className="w-full h-3 mt-2 rounded-sm bg-zinc-300 animate-pulse" />
