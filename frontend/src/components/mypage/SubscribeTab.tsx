aaf5910480221349debe94f31f6fdb6115b3e568
Yena Lee <lmonari71@gmail.com>
2022-12-22 02:28:27 +0900

    feat : 유저 구독 정보 로직 생성

--- frontend/src/components/mypage/SubscribeTab.tsx
+++ frontend/src/components/mypage/SubscribeTab.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
 import { CardType, TextType, WrapperType } from '../common/theme';
 import { CardLayout } from '../layout/GlobalLayout';
 import { MyPageContentsLayout } from '../layout/MyPageLayout';
+import { AuthStore } from '../../stores/UserStore';
+
+const serverURL = 'http://localhost:3000';
+
+interface IUserSubscriptionData {
+  id: string;
+  categoryName: string;
+  mascotName: string;
+  mascotImage: string;
+}
+const getUserSubscription = async (accessToken: string | null) => {
+  const response = await axios.get(`${serverURL}/subscribe`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return response.data;
+};
 
 const SubscribeTab = () => {
+  const navigate = useNavigate();
+  const [userCardInfo, setUserCardInfo] = useState<IUserSubscriptionData | null>(null);
+  const accessToken = AuthStore((state) => state.token);
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigate('/login');
+    }
+    const data = getUserSubscription(accessToken);
+    data.then((res) => {
+      setUserCardInfo(res);
+    });
+  }, []);
+  console.log(userCardInfo);
   const tabCategories = { subscribed: ['뉴스', '푸드', '라이프스타일', '후원', '토픽'] };
   const tabCards = tabCategories.subscribed.map((card, index) => (
     <li className={CardType.size} key={index}>
