d606be73dda0f3e9dca026166b45016a2839a0d4
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-21 18:30:25 +0900

    feat : zodSchema로 validation

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,33 +1,34 @@
 import { Request, Response } from 'express';
 import { UserService } from '../user/user.service';
+import { zParse } from '../zParse';
 import { AuthService } from './auth.service';
+import { registerSchema, loginSchema, isDuplicatedSchema } from './auth.zodSchema';
 
 const authService = new AuthService();
 const userService = new UserService();
 
 export class AuthController {
   async register(req: Request, res: Response) {
-    await userService.register(req.body);
+    const { body } = await zParse(registerSchema, req);
+
+    await userService.register(body);
     res.status(201).end();
   };
 
   async login(req: Request, res: Response) {
-    const { email, password } = req.body;
-    const userToken = await authService.generateUserToken({ email, password });
+    const { body } = await zParse(loginSchema, req);
+    const userToken = await authService.generateUserToken(body);
     res.status(200).json(userToken);
   };
 
   async isDuplicated(req: Request, res: Response) {
-    const { email, username } = req.body;
-    const result = { } as Partial<{
-      email: boolean;
-      username: boolean;
-    }>;
-    if (email) {
-      result.email = await userService.isDuplicatedEmail(email);
+    const { body } = await zParse(isDuplicatedSchema, req);
+    const result = { } as Partial<{ email: boolean, username: boolean }>;
+    if (body.email) {
+      result.email = await userService.isDuplicatedEmail(body.email);
     }
-    if (username) {
-      result.username = await userService.isDuplicatedUsername(username);
+    if (body.username) {
+      result.username = await userService.isDuplicatedUsername(body.username);
     }
     res.status(200).json(result);
   }
