bab2634e744556b0ec62d232fbd9ea3c17241874
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2022-12-22 18:06:02 +0900

    fix: useCategory hook 수정

--- frontend/src/hooks/useCategory.ts
+++ frontend/src/hooks/useCategory.ts
@@ -1,12 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
+interface ICategory {
+  _id: string;
+  categoryName: string;
+  mascotName: string;
+  mascotImage: string;
+  posts: any[];
+  __v: number;
+}
+
 export default function useCategory() {
-  const catQuery = useQuery({
+  const catQuery = useQuery<ICategory[]>({
     queryKey: ['categories'],
     queryFn: async () => {
-      return axios.get('/category').then((reponse) => reponse.data);
+      return axios.get('/category').then((res) => res.data);
     },
+    staleTime: 1000 * 60,
   });
 
   return { catQuery };
--- frontend/src/pages/CategoriesPage.tsx
+++ frontend/src/pages/CategoriesPage.tsx
@@ -4,6 +4,7 @@ import { CardLayout } from '../components/layout/GlobalLayout';
 import CardSkeleton from '../components/loadings/CardSkeleton';
 import { CardType, TextType, WrapperType } from '../components/common/theme';
 import useCategory from '../hooks/useCategory';
+import { checkName } from '../util/functionUtil';
 
 export default function CategoriesPage() {
   const {
@@ -23,13 +24,13 @@ export default function CategoriesPage() {
           {categories &&
             categories.map((category) => (
               <li className={CardType.size} key={category._id}>
-                <Link to={`/categories/${category.id}`} className={CardType.layout}>
+                <Link to={`/categories/${category.categoryName}`} className={CardType.layout}>
                   <div className={CardType.imgWrapper}>
                     <img className={CardType.img} src={category.mascotImage} alt="default card" />
                   </div>
                   <div className={CardType.text}>
                     <h2>
-                      {category.mascotName}이 전하는 오늘의 {category.categoryName}{' '}
+                      {checkName(category.mascotName)} 전하는 오늘의 {category.categoryName}{' '}
                     </h2>
                   </div>
                 </Link>
