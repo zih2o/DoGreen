c57decce945ae7a7c5cdbc9d0b293b9ce5f0a2c4
Yena Lee <lmonari71@gmail.com>
2022-12-23 03:13:26 +0900

    feat : user message 스켈레톤 코드 추가

--- frontend/src/components/loadings/ProfileSkeleton.tsx
+++ frontend/src/components/loadings/ProfileSkeleton.tsx
@@ -5,11 +5,13 @@ export const ProfileSkeleton = () => {
     <>
       <section className={WrapperType.profileTotalWrapper + 'my-12'}>
         <div className={WrapperType.profileWrapper}>
-          <div className={'w-20 h-20 xl:w-28 xl:h-28 rounded-full p-2 m-4 bg-zinc-300 animate-pulse'}></div>
+          <div
+            className={'w-20 h-20 xl:w-28 xl:h-28 rounded-full p-2 m-4 bg-zinc-300 dark:bg-[#292524] animate-pulse'}
+          ></div>
         </div>
         <div className={WrapperType.textWrapper + 'px-3'}>
-          <p className="w-full h-4 mb-2 rounded-sm bg-zinc-300 animate-pulse"></p>
-          <p className="w-full h-4 rounded-sm bg-zinc-300 animate-pulse"></p>
+          <p className="w-full h-4 mb-2 rounded-sm bg-zinc-300 dark:bg-[#292524] animate-pulse"></p>
+          <p className="w-full h-4 rounded-sm bg-zinc-300 dark:bg-[#292524] animate-pulse"></p>
         </div>
       </section>
     </>
--- frontend/src/components/mypage/UserMessage.tsx
+++ frontend/src/components/mypage/UserMessage.tsx
@@ -3,6 +3,7 @@ import { TextType } from '../common/theme';
 import { MyPageMessageLayout } from '../layout/MyPageLayout';
 import { AuthStore } from '../../hooks/useAuth';
 import useUserData from '../../hooks/useUserData';
+import { MessageSkeleton } from '../loadings/MessageSkeleton';
 
 export const UserMessage = () => {
   const accessToken = AuthStore((state) => state.token);
@@ -11,15 +12,19 @@ export const UserMessage = () => {
   } = useUserData(accessToken);
 
   return (
-    <MyPageMessageLayout>
-      {userData && (
-        <>
-          <span className={TextType.messageText + ' empty:hidden'}>{userData.username}</span>님, 상위 60%의
-          <span className={TextType.messageText}> Earth Guardian</span>
-          이군요!
-          <br></br> 현재까지 <span className={TextType.messageText}>60마리</span>의 아델리펭귄을 구하셨어요.
-        </>
-      )}
-    </MyPageMessageLayout>
+    <>
+      {isLoading && <MessageSkeleton />}
+
+      <MyPageMessageLayout>
+        {userData && (
+          <>
+            <span className={TextType.messageText}>{userData.username}</span>님, 상위 60%의
+            <span className={TextType.messageText}> Earth Guardian</span>
+            이군요!
+            <br></br> 현재까지 <span className={TextType.messageText}>60마리</span>의 아델리펭귄을 구하셨어요.
+          </>
+        )}
+      </MyPageMessageLayout>
+    </>
   );
 };
