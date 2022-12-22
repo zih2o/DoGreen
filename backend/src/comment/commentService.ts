fd6f010b598bedfb36984aefb26c627e72b6bea0
Son HyeongSeok <74246053+sonhs1996@users.noreply.github.com>
2022-12-23 00:50:34 +0900

    feat : commit

--- backend/src/comment/commentService.ts
+++ backend/src/comment/commentService.ts
@@ -5,8 +5,12 @@ const commentRepository = new CommentRepository();
 const postRepository = new PostRepository();
 
 export class CommentService {
-    async createComment(comment: CommentT['content'], id: PostT['id']) {
-      const findIdForComment = await postRepository.findPost(id);
+    async createComment(comment: CommentT['content'], id: PostT['id'], username: UserT['username']) {
+        // 코멘트 등록
+        // 포스트의 아이디를 찾아 넣어줘야한다
+        // 
+        const findIdForComment = await postRepository.findPost(id);
+        const findNameForComment = await
       await commentRepository.createComment(findIdForComment?.id, comment);
       
 }
