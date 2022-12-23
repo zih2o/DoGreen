28746d056684a93e0b80459619083bf3a92c95c0
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-23 21:08:44 +0900

    fix : imageRouter에 loginRequired 추가

--- backend/src/image/image.router.ts
+++ backend/src/image/image.router.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { ImageController } from './image.controller';
 import { nextError } from '../nextError';
+import { loginRequired } from '../middleware/loginRequired';
 
 const imageController = new ImageController();
 const imageRouter = Router();
+
+imageRouter.use(loginRequired);
 
 imageRouter.post('/url', nextError(imageController.generateUrl));
 
