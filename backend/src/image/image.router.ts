dc0ecbf2e1dd4077b7b258c50b5adfcdb1f5b866
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-24 21:10:06 +0900

    fix : imageRouter, 업로드 middleware 타입 에러 수정, env가 undefined인 에러 수정

--- backend/src/image/image.router.ts
+++ backend/src/image/image.router.ts
@@ -1,11 +1,12 @@
-import { Router } from 'express';
+import express from 'express';
+import upload from './upload.middleware';
 import { ImageController } from './image.controller';
 import { nextError } from '../nextError';
-import { loginRequired } from '../middleware/loginRequired';
 
 const imageController = new ImageController();
-const imageRouter = Router();
-imageRouter.use(loginRequired);
-imageRouter.post('/upload', nextError(imageController.uploadFileToS3));
+
+const imageRouter = express.Router();
+
+imageRouter.post('/', upload.single('image'), nextError(imageController.post));
 
 export { imageRouter };
--- backend/src/image/upload.middleware.ts
+++ backend/src/image/upload.middleware.ts
@@ -1,22 +1,30 @@
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 import { S3Client } from '@aws-sdk/client-s3';
+import type { AwsCredentialIdentity } from '@aws-sdk/types';
 import { Request } from 'express';
+import dotenv from 'dotenv';
 import invariant from '../invariant';
+import logger from '../logger';
+
+dotenv.config(); // 꼼수!
 
 type FileNameCallback = (error: Error | null, filename: string) => void;
 
-invariant(process.env.AWS_ACCESS_KEY, 'ACESS_KEY 환경변수가 필요합니다.');
-invariant(process.env.AWS_SECRET_KEY, 'SECRET_KEY 환경변수가 필요합니다.');
+invariant(process.env.AWS_ACCESS_KEY, 'AWS_ACCESS_KEY 환경변수가 필요합니다.');
+invariant(process.env.AWS_SECRET_KEY, 'AWS_SECRET_KEY 환경변수가 필요합니다.');
 invariant(process.env.BUCKET_NAME, 'BUCKET_NAME 환경변수가 필요합니다.');
 
+const credentials: AwsCredentialIdentity & { accessKeyId: string } = {
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY
+};
+
 const s3Client = new S3Client({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY
-  },
+  credentials,
   region: 'ap-northeast-2'
 });
+logger.debug(JSON.stringify(credentials));
 
 const upload = multer({
   storage: multerS3({
@@ -25,8 +33,9 @@ const upload = multer({
     acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key(req: Request, file: Express.Multer.File, cb: FileNameCallback) {
-      cb(null, `${Date.now()}_${file.originalname}`);
+      cb(null, `${Date.now()}-${encodeURI(file.originalname)}`);
     }
-  })
+  }),
+  limits: { fileSize: 20 * 1024 * 1024 } // 20MB
 });
 export default upload;
