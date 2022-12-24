964eea133bdaec9c727e016582c56eee6b3b789f
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-24 21:07:50 +0900

    fix : imageController 에러 수정

--- backend/src/image/image.controller.ts
+++ backend/src/image/image.controller.ts
@@ -3,18 +3,14 @@ import {
 } from 'express';
 import { BadRequestError } from '../errors/BadRequestError';
 import invariant from '../invariant';
-import { ImageService } from './image.service';
 
-const imageService = new ImageService();
 export class ImageController {
-  async uploadFileToS3(req: Request, res: Response) {
+  async post(req: Request, res: Response) {
     invariant(
       req.file !== null && req.file !== undefined,
       new BadRequestError('이미지 파일이 필요합니다.')
     );
 
-    const fileData: Express.Multer.File = req.file;
-    await imageService.uploadFileToS3(fileData);
-    res.status(201).end();
+    res.status(200).send(req.file.location);
   }
 }
