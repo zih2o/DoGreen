68fcc368c0f0300b8cbf0ebaacf976025a69e411
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-24 21:07:00 +0900

    fix : 사용하지 않는 temp 파일 제거

--- backend/src/multerS3/image.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import upload from './upload.middleware';
-import { ImageController } from './image.controller';
-
-const imageController = new ImageController();
-
-export const router = express.Router();
-
-router.post('/image', upload.single('image'), imageController.post);
