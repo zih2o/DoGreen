63be90e428acdd2d5ca3ae532b21aaabb6b01555
SIYEA J <siyea0405@gmail.com>
2022-12-25 00:59:44 +0900

    feat: User의 Role이 ADMIN이 아니면 /admin 에 접근불가

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import AdminBody from '../components/AdminBody';
+import useUserData from '../hooks/useUserData';
+import { AuthStore } from '../hooks/useAuth';
+import Loading from '../components/loadings/Loading';
+import NotFound from './NotFound';
+
 function Admin() {
+  const accessToken = AuthStore((state) => state.token);
+  const {
+    userQuery: { isLoading, data: userData },
+  } = useUserData(accessToken);
+
   return (
     <div className="w-full min-h-screen grid grid-cols-10 grid-rows-6">
       <div className="grid grid-cols-10 grid-rows-6 mt-[113.99px] mb-[92px] col-span-full row-span-6 sm:mb-[72px]">
-        <AdminBody/>
+        {isLoading ? <Loading /> : userData?.role === 'ADMIN' ? <AdminBody /> : <NotFound />}
       </div>
     </div>
   );
 }
-export default Admin;  
+export default Admin;
