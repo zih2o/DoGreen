d3f41d92f9a7630e10c7d422a534e71c32052b4d
Son HyeongSeok <74246053+sonhs1996@users.noreply.github.com>
2022-12-25 16:50:55 +0900

    feat : comment pagination

--- backend/src/comment/commentController.ts
+++ backend/src/comment/commentController.ts
@@ -4,6 +4,15 @@ import { CommentService } from './commentService';
 const commentService = new CommentService();
 
 export class CommentController {
+  async paginationComment(req:Request, res:Response, next:NextFunction) {
+    const { postId } = req.query;
+    const page = Number(req.query.page || 1);
+    const perPage = Number(req.query.perPage || 10);
+
+    const pagingComments = await commentService.paginationPost(postId, page, perPage);
+    res.status(201).json(pagingComments);
+  }
+
   async findAllCommentAtPost(req: Request, res: Response, next: NextFunction) {
     const postId = req.params;
     const allComment = await commentService.findAllCommentAtPost(postId);
