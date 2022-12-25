549d7a0e79f307dbe1de67320b6b3e42bd5cc597
SIYEA J <siyea0405@gmail.com>
2022-12-26 00:27:23 +0900

    feat: Loading 컨테이너 추가

--- frontend/src/components/Hamburger.tsx
+++ frontend/src/components/Hamburger.tsx
@@ -4,29 +4,23 @@ import Categorys from './wastebasket/X_Category2';
 import LoginForm from './hamburger/LoginForm';
 import { useHamburgerStore } from '../hooks/useHamburger';
 
-function Hamburger(props) {
+import Loading from './loadings/Loading';
+
+import useUserData from '../hooks/useUserData';
+import { AuthStore } from '../hooks/useAuth';
+
+function Hamburger() {
   const { hamburgerOpen, toggleHamburger } = useHamburgerStore();
   const [handleModal, setHandleModal] = useState(hamburgerOpen);
   const closeModal = () => {
     toggleHamburger();
     setHandleModal(!handleModal);
   };
+  const accessToken = AuthStore((state) => state.token);
+  const {
+    userQuery: { data: userData },
+  } = useUserData(accessToken);
   const isLogined = window.sessionStorage.getItem('token');
-  // const initialLoginBtn = isLogined ? '로그아웃' : '로그인';
-  // const [loginStatus, setLoginStatus] = useState<string>(initialLoginBtn);
-  // const logined = () => {
-  //   if (isLogined === null) {
-  //     setLoginStatus('로그아웃');
-  //     setHandleModal(!handleModal); //로그인 창 열기
-  //   } else {
-  //     setLoginStatus('로그인');
-  //     window.sessionStorage.clear();
-  //   }
-  // };
-  // useEffect(() => {
-  //   //제일처음할것
-  //   //localstor
-  // })
   const className = {
     topDiv:
       '-z-10 p-6 fixed top-[12.3%] bottom-[100px] right-[0%] w-[70%] h-full items-center place-content-between bg-navbarBG text-right border-b-2 border-garden4 md:w-[30%]',
@@ -38,11 +32,11 @@ function Hamburger(props) {
           <>
             <div className="flex flex-col mx-auto items-center">
               <div className="leading-10 text-garden2 text-3xl">
-                {props.rank} {props.name}님
+                {userData ? (`${userData.role} ${userData.username}님`) : <Loading/>}
               </div>
               <span className=" text-garden2 text-xl">지금까지 펭귄 300마리를 구하셨어요!</span>
             </div>
-            <Categorys name="nav" />
+            <Categorys name="nav"/>
           </>
         ) : (
           <LoginForm />
