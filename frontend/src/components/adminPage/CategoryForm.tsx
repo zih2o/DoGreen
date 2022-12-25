664b48e46f3c6644540f7518d0f081f61567cd91
SIYEA J <siyea0405@gmail.com>
2022-12-26 00:20:52 +0900

    feat: api Create 요청 기능 완

--- frontend/src/components/adminPage/CategoryForm.tsx
+++ frontend/src/components/adminPage/CategoryForm.tsx
@@ -1,27 +1,48 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
+import { api } from '../../util/api';
+import { useMutation } from '@tanstack/react-query';
 
 export default function CategoryForm() {
-  const [formValue, setformValue] = useState({
-    category: '',
-    content: '',
-  });
+  const mascotNameRef = useRef();
+  const categoryRef = useRef();
+  const mutation = useMutation((data) => api.post('/category/create', data));
 
   const handleSubmit = (event) => {
-    // we will fill this in the coming paragraph
-  };
-
-  const handleChange = (event) => {
-    setformValue({
-      ...formValue,
-      [event.target.name]: event.target.value,
-    });
+    const formData = {
+      mascotName: mascotNameRef.current.value,
+      categoryName: categoryRef.current.value,
+    };
+    confirm(`${formData.mascotName} 카테고리를 저장하시겠습니까?`) ? mutation.mutate(formData) : event.preventDefault();
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input placeholder="동물이름을 적어주세요" required className="my-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
-      <input placeholder="카테고리 이름을 적어주세요" required className="my-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
-      <button type="submit" className="p-2.5 rounded-md justify-center border-2 border-garden4 hover:bg-garden4 hover:text-white" onClick={handleSubmit}>
+    <form className="px-7" onSubmit={handleSubmit}>
+      <label className="flex text-sm" htmlFor="categoryMascotNameInput">
+        Mascot Name
+      </label>
+      <input
+        ref={mascotNameRef}
+        id="categoryMascotNameInput"
+        type="text"
+        placeholder="동물이름을 적어주세요"
+        required
+        className="mb-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+      />
+      <label className="flex text-sm" htmlFor="categoryNameInput">
+        Category Name
+      </label>
+      <input
+        ref={categoryRef}
+        id="categoryNameInput"
+        type="text"
+        placeholder="카테고리 이름을 적어주세요"
+        required
+        className="mb-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+      />
+      <button
+        type="submit"
+        className="p-2.5 rounded-md justify-center border-2 border-garden4 hover:bg-garden4 hover:text-white"
+      >
         Submit
       </button>
     </form>
