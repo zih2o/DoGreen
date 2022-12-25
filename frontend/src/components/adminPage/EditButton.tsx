d86e70cadd16a5638ee8d1fa4a856a7e572dc1b1
SIYEA J <siyea0405@gmail.com>
2022-12-26 00:27:50 +0900

    feat: pull 전 임시저장

--- frontend/src/components/adminPage/EditButton.tsx
+++ frontend/src/components/adminPage/EditButton.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-export default function EditButton() {
-
+export default function EditButton({id}) {
   return (
     <div
       id="dropdown"
-      className="absolute top-11 w-36 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700"
+      className="relative top-11 w-36 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700"
     >
       <ul className="py-1" aria-labelledby="dropdownButton">
-        <Link
-          to="#"
-          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-        >
+        <div className="cursor-pointer block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
           <li>Edit</li>
-        </Link>
+        </div>
 
-        <Link
-          to="#"
-          className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-        >
+        <div className="cursor-pointer block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
           <li>Delete</li>
-        </Link>
+        </div>
       </ul>
     </div>
   );
