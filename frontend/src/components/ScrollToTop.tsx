fc164ac1ee66886e9b9e1158fee87b6b8e476470
Yena Lee <lmonari71@gmail.com>
2022-12-26 01:16:08 +0900

    feat : ToTopBtn 생성, 홈화면 추가

--- frontend/src/components/ScrollToTop.tsx
+++ frontend/src/components/ScrollToTop.tsx
@@ -1,6 +1,21 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { GrLinkTop } from 'react-icons/gr';
 
+export const MoveToTop = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+export const ToTopButton = () => {
+  return (
+    <button
+      id="to-top-button"
+      onClick={MoveToTop}
+      className="flex justify-center items-center fixed z-[90] bottom-20 right-5 border-0 w-14 h-14 rounded-full drop-shadow-md bg-slate-50 dark:bg-slate-100 "
+    >
+      <GrLinkTop size="32" />
+    </button>
+  );
+};
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
