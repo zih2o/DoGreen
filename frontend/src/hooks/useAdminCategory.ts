8a04a73d09711c2101e50e33d6f38728dee471f8
SIYEA J <siyea0405@gmail.com>
2022-12-26 07:00:53 +0900

    feat: Modal에서 사용할 전역상태 hook 추가

--- frontend/src/hooks/useAdminCategory.ts
+++ frontend/src/hooks/useAdminCategory.ts
@@ -7,14 +7,26 @@ interface IAdminCategory {
 
   adminCreateCardBtn: boolean;
   toggleAdminCreateCardBtn: () => void;
-  closeAdminCreateCardBtn:() => void;
+  closeAdminCreateCardBtn: () => void;
+
+  adminModal: boolean;
+  toggleAdminModal: () => void;
+  closeAdminModal: () => void;
+
+  currentCategoryCard: { mascotName: string; categoryName: string };
 }
 export const useAdminCategoryStore = create<IAdminCategory>((set) => ({
   adminCategory: 'Mascot',
   setMascotCategory: () => set(() => ({ adminCategory: 'Mascot' })),
   setNewsCategory: () => set(() => ({ adminCategory: 'News' })),
-  
+
   adminCreateCardBtn: false,
   toggleAdminCreateCardBtn: () => set((state) => ({ adminCreateCardBtn: !state.adminCreateCardBtn })),
   closeAdminCreateCardBtn: () => set(() => ({ adminCreateCardBtn: false })),
+
+  adminModal: false,
+  toggleAdminModal: () => set((state) => ({ adminModal: !state.adminModal })),
+  closeAdminModal: () => set(() => ({ adminModal: false })),
+
+  currentCategoryCard: { mascotName: '', categoryName: '' },
 }));
