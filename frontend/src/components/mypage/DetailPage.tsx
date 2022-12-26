6b0d49e9521d94c21e37c1f8d98aa30bfff107d3
지현 이 <rachel2148072@gmail.com>
2022-12-26 16:02:29 +0000

    feat & recator & fix : 2주차 코드리뷰 반영 및 회원탈퇴, 다크모드, 이미지업로드 반영

--- frontend/src/components/mypage/DetailPage.tsx
+++ frontend/src/components/mypage/DetailPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MyPageMain from './MyPageMain';
-import { FormEditUserInfo } from './EditUserInfo';
+import EditUserInfo from './EditUserInfo';
 import SubscribeTab from './SubscribeTab';
 import ChallengeInfo from './ChallengeInfo';
 
@@ -8,7 +8,7 @@ export const MyHome = () => {
   return <MyPageMain />;
 };
 export const MyInfoEditTab = () => {
-  return <FormEditUserInfo />;
+  return <EditUserInfo />;
 };
 export const MySubscribe = () => {
   return <SubscribeTab />;
--- frontend/src/hooks/authApi.ts
+++ frontend/src/hooks/authApi.ts
@@ -1,8 +1,9 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { api } from '../util/api';
 import { useNavigate } from 'react-router';
+import { AlertModal } from '../components/common/AlertModal';
 
-export interface IAuthInput {
+export interface IAuthData {
   username?: string;
   email: string;
   password: string;
@@ -11,13 +12,16 @@ export interface IAuthInput {
 export function useLogin() {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-  const registerMutate = async (data: IAuthInput) => {
+  const registerMutate = async (data: IAuthData) => {
     const res = await api.post('/auth/login', data);
     const token = res.data;
     return token;
   };
-  const mutation = useMutation({
+  return useMutation({
     mutationFn: registerMutate,
+    onMutate: () => {
+      console.log('test');
+    },
     onSuccess: (token) => {
       queryClient.invalidateQueries({ queryKey: ['user'] });
       console.log('성공', token);
@@ -26,19 +30,18 @@ export function useLogin() {
     },
     onError: (error) => {
       console.error('에러 발생했지롱');
-      //error 에서 메시지 가져오는 법.....어떻게 타입오류 제거하지??
       alert(error?.response?.data?.error);
+      console.log('test');
     },
   });
-  return { mutation };
 }
 
 export function useResiter() {
   const queryClient = useQueryClient();
-  const registerMutate = async (data: IAuthInput) => {
+  const registerMutate = async (data: IAuthData) => {
     return await api.post('/auth/register', data);
   };
-  const mutation = useMutation({
+  return useMutation({
     mutationFn: registerMutate,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['user'] });
@@ -49,28 +52,4 @@ export function useResiter() {
       alert(error?.response?.data?.error);
     },
   });
-  return { mutation };
-}
-
-export function useWithDrawData() {
-  const navigate = useNavigate();
-  const withdrawMutate = async (currentPassword: string) => {
-    return await api.patch('/user/me/withdraw', currentPassword);
-  };
-  const mutation = useMutation({
-    mutationFn: withdrawMutate,
-    onMutate: () => {
-      alert('탈퇴하시겠습니까?');
-    },
-    onSuccess: () => {
-      console.log('회원탈퇴');
-      alert('회원탈퇴 되었습니다.');
-      navigate('/');
-    },
-    onError: (error) => {
-      console.error('에러 발생했지롱');
-      alert(error?.response?.data?.error);
-    },
-  });
-  return { mutation };
 }
