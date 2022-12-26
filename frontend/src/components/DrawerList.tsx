26b4953f321d80819ecd785f2614768158226157
SIYEA J <siyea0405@gmail.com>
2022-12-27 07:30:58 +0900

    fix: Drawer 이름변경, user role에 따른 Draswer List 기능추가

---
+++ frontend/src/components/DrawerList.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface IDrawerNav {
+  name: 'user' | 'admin';
+  handleModal: () => void;
+}
+
+interface IDrawerNaveList {
+  name: string;
+  url: string;
+}
+
+type DrawerNameType = 'user' | 'admin';
+type DrawerDrawerDatasType = Record<DrawerNameType, IDrawerNaveList[]>;
+
+export default function DrawerList({ name, handleModal }: IDrawerNav) {
+  const DrawerDrawerDatas: DrawerDrawerDatasType = {
+    user: [
+      { name: '카드 보러가기', url: '#' },
+      { name: '마이페이지', url: '/mypage' },
+    ],
+    admin: [
+      { name: '카드 보러가기', url: '#' },
+      { name: '마이페이지', url: '/mypage' },
+      { name: '관리자페이지', url: '/admin' },
+    ],
+  };
+  const drawerDatas = DrawerDrawerDatas[name];
+  return (
+    <>
+      <ul className={`flex flex-col ${name === 'admin' && "pt-16"}`}>
+        {name === 'user' && (
+          <>
+            <button className="my-4 text-garden2 bg-garden1 hover:bg-garden2 hover:text-garden1 border-2 border-garden1 text-lg font-bold rounded-lg w-95% px-5 py-2 text-center dark:border-forest3 dark:bg-forest3 dark:text-forest4 dark:hover:border-forest4 dark:hover:bg-forest4 dark:hover:text-forest3">
+              MY 구독
+            </button>
+            <button className="my-4 text-garden2 bg-garden1 hover:bg-garden2 hover:text-garden1 border-2 border-garden1 text-lg font-bold rounded-lg w-95% px-5 py-2 text-center dark:border-forest3 dark:bg-forest3 dark:text-forest4 dark:hover:border-forest4 dark:hover:bg-forest4 dark:hover:text-forest3">
+              MY 뉴스레터
+            </button>
+          </>
+        )}
+        {drawerDatas?.map((data: IDrawerNaveList) => {
+          return (
+            <Link to={data.url} key={data.name} className="hover:underline" onClick={handleModal}>
+              <li className="text-l mt-5 md:text-xl font-bold text-garden2 dark:text-gray-200">{data.name}</li>
+            </Link>
+          );
+        })}
+        <div className='pt-20'>
+          <Link to="/login" className="hover:underline" onClick={handleModal}>
+            <li className='text-l mt-5 dark:text-gray-200 md:text-xl font-bold text-garden2'>로그아웃</li>
+          </Link>
+        </div>
+      </ul>
+    </>
+  );
+}
--- frontend/src/components/hamburger/LoginForm.tsx
+++ frontend/src/components/hamburger/LoginForm.tsx
@@ -1,27 +1,22 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { useHamburgerStore } from '../../hooks/useHamburger';
-
+import { useDrawerStore } from '../../hooks/useDrawer';
+import LoginModalPage from '../../pages/LoginModalPage';
 export default function LoginForm() {
-  const { hamburgerOpen, toggleHamburger } = useHamburgerStore();
-  const [handleModal, setHandleModal] = useState<boolean>(false);
-  const closeModal = () => {
-    setHandleModal(!handleModal);
-  };
+  const { drawerOpen, toggleDrawer } = useDrawerStore();
 
   return (
     <>
       <button
         className="block mb-4 text-garden2 bg-navbarBG hover:bg-garden1  border-2 border-garden1 text-lg font-bold rounded-lg w-full px-5 py-2 text-center dark:border-forest3 dark:text-forest3 dark:hover:bg-forest3 dark:hover:text-forest4"
-        onClick={toggleHamburger}
+        onClick={toggleDrawer}
       >
         로그인
       </button>
       <Link
         to="/register"
         className="block mb-4 text-garden2 hover:underline text-lg font-bold w-full px-5 py-2 text-center dark:text-forest3"
-        onClick={toggleHamburger}
+        onClick={toggleDrawer}
       >
         회원가입
       </Link>
