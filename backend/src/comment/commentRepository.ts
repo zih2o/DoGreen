6b076b3215da7f90de9f7f2e2d88a5f93715a4b1
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-27 22:11:59 +0900

    fix : comment pagination 추가

--- backend/src/comment/commentRepository.ts
+++ backend/src/comment/commentRepository.ts
@@ -3,10 +3,12 @@ import { CommentSchema } from './commentSchema';
 import { PostSchema } from '../post/postSchema';
 import invariant from '../invariant';
 import { NotFoundError } from '../errors/NotFoundError';
+import ApplicationError from '../errors/ApplicationError';
+import { UserSchema } from '../user/user.schema';
 
 const CommentModel = model<CommentT>('comments', CommentSchema);
 const PostModel = model<PostT>('posts', PostSchema);
-
+const UserModel = model<UserT>('users', UserSchema);
 export class CommentRepository implements ICommentRepository {
   async paginationComment(postId: string, page: number, perPage: number) {
     // eslint-disable-next-line max-len
@@ -21,12 +23,36 @@ export class CommentRepository implements ICommentRepository {
     });
 
     invariant(post !== null, new NotFoundError('해당하는 포스트가 존재하지 않습니다.'));
+    invariant(post.comments !== undefined, new ApplicationError('해당하는 댓글이 존재하지 않습니다.', 404));
 
     const total = await CommentModel.count({ post: postId });
     const totalPage = Math.ceil(total / perPage);
+    const authIdList = post.comments.map(comment => comment.authId);
+
+    const userList = await UserModel.find({ auth: authIdList }, undefined, {
+      select: 'auth username imgUrl'
+    });
+
+    const userMap = new Map<string, Pick<UserT, 'username' | 'imgUrl'> & { _id: string }>();
+    userList.forEach(user => {
+      const authId = String(user.auth);
+      userMap.set(authId, {
+        _id: String(user._id),
+        username: user.username,
+        imgUrl: user.imgUrl
+      });
+    });
+    const result = post.comments.map((comment, i) => ({
+      _id: comment._id,
+      refPost: comment.refPost,
+      userId: userMap.get(String(authIdList[i])),
+      comment: comment.comment,
+      createdAt: comment.createdAt,
+      updatedAt: comment.updatedAt
+    }));
 
     return {
-      page, perPage, result: post.comments, totalPage
+      page, perPage, result, totalPage
     };
   }
 
