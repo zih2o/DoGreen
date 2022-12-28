ae0740bdf7c51c6f8f18f187e4e0e4d815184f07
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-28 21:30:59 +0900

    feat : loginOptional 미들웨어 구현

--- backend/src/middleware/adminRequired.ts
+++ backend/src/middleware/adminRequired.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { ForbiddenError } from '../errors/ForbiddenError';
+import invariant from '../invariant';
 
 const adminRequired = (req: Request, res: Response, next: NextFunction) => {
+  invariant(req.context.currentUser !== undefined, new ForbiddenError('로그인해야 이용할 수 있는 서비스입니다.'));
   if (req.context.currentUser.role !== 'ADMIN') {
     throw new ForbiddenError('관리자만 이용할 수 있습니다.');
   }
