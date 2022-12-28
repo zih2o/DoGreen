6ccf59a32f932068959b65132374f162d269fbca
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-28 22:22:48 +0900

    fix : post Router에 loginOptional 미들웨어 추가

--- backend/src/post/postRouter.ts
+++ backend/src/post/postRouter.ts
@@ -3,6 +3,7 @@ import { PostController } from './postController';
 import { adminRequired } from '../middleware/adminRequired';
 import { loginRequired } from '../middleware/loginRequired';
 import { nextError } from '../nextError';
+import { loginOptional } from '../middleware/loginOptional';
 
 const postController = new PostController();
 const postRouter = Router();
@@ -11,7 +12,7 @@ postRouter.post('/create', loginRequired, adminRequired, nextError(postControlle
 postRouter.get('/like/:id', loginRequired, nextError(postController.addlikeUserId));
 postRouter.patch('/:id', loginRequired, adminRequired, nextError(postController.updatePost));
 postRouter.delete('/:id', loginRequired, adminRequired, nextError(postController.deletePost));
-postRouter.get('/', loginRequired, nextError(postController.paginationPost));
+postRouter.get('/', loginOptional, nextError(postController.paginationPost));
 postRouter.get('/:id', nextError(postController.findOnePost));
 // postRouter.get('/', nextError(postController.findAllPost));
 
