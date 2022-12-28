ae09a808d7a9091c8132f1d302d45c8ccaa2ae6e
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-29 03:25:10 +0900

    feat : router에 loginOptional 추가

--- backend/src/post/postRouter.ts
+++ backend/src/post/postRouter.ts
@@ -8,12 +8,17 @@ import { loginOptional } from '../middleware/loginOptional';
 const postController = new PostController();
 const postRouter = Router();
 
+// ADMIN
 postRouter.post('/create', loginRequired, adminRequired, nextError(postController.createPost));
-postRouter.get('/like/:id', loginRequired, nextError(postController.addlikeUserId));
 postRouter.patch('/:id', loginRequired, adminRequired, nextError(postController.updatePost));
 postRouter.delete('/:id', loginRequired, adminRequired, nextError(postController.deletePost));
+postRouter.get('/', loginRequired, adminRequired, nextError(postController.findAllPost));
+
+// USER
+postRouter.get('/like/:id', loginRequired, nextError(postController.addlikeUserId));
+
+// USER | NOT LOGGED IN
 postRouter.get('/', loginOptional, nextError(postController.paginationPost));
-postRouter.get('/:id', nextError(postController.findOnePost));
-// postRouter.get('/', nextError(postController.findAllPost));
+postRouter.get('/:id', loginOptional, nextError(postController.findOnePost));
 
 export { postRouter };
