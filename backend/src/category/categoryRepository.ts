31cfd0351b1b0c1c64b442b308b0075635866164
chloeseo0210 <chloeseo0210@gmail.com>
2022-12-29 00:40:19 +0900

    fix : 에러처리 수정

--- backend/src/category/categoryRepository.ts
+++ backend/src/category/categoryRepository.ts
@@ -1,28 +1,33 @@
-import { model, Types } from 'mongoose';
+import { model } from 'mongoose';
 import ApplicationError from '../errors/ApplicationError';
 import { BadRequestError } from '../errors/BadRequestError';
+import { InternalServerError } from '../errors/InternalServerError';
+import { NotFoundError } from '../errors/NotFoundError';
 import invariant from '../invariant';
+import { PostSchema } from '../post/postSchema';
 import { CategorySchema } from './categorySchema';
 
+const PostModel = model<PostT>('posts', PostSchema);
 const CategoryModel = model<categoryT>('categories', CategorySchema);
 
 export class CategoryRepository implements ICategoryRepository {
   async findCategory(categoryName: string) {
     // id필드값만 반환하게 하면 좀 더 메모리가 덜 들 것 같은데 추후에..
     const category = await CategoryModel.exists({ categoryName });
-    invariant(category !== null, new ApplicationError('id에 해당하는 카테고리가 존재하지 않습니다.', 404));
+    invariant(category !== null, new NotFoundError('id에 해당하는 카테고리가 존재하지 않습니다.'));
     return category._id;
   }
 
   async updateOneCategory(toUpdate:updateCategoryDto, id: string) {
     const category = await CategoryModel.exists({ _id: id });
-    invariant(category !== null, new ApplicationError('id에 해당하는 카테고리가 존재하지 않습니다.', 404));
+    invariant(category !== null, new NotFoundError('id에 해당하는 카테고리가 존재하지 않습니다.'));
     await CategoryModel.updateOne({ _id: id }, toUpdate);
   }
 
   async deleteOneCategory(categoryId: string) {
-    const category = await CategoryModel.exists({ _id: categoryId });
-    invariant(category !== null, new ApplicationError('id에 해당하는 카테고리가 존재하지 않습니다.', 404));
+    const category = await CategoryModel.findById(categoryId);
+    invariant(category !== null, new NotFoundError('id에 해당하는 카테고리가 존재하지 않습니다.'));
+    await PostModel.deleteMany({ _id: category.posts });
     await CategoryModel.deleteOne({ _id: categoryId });
   }
 
@@ -41,8 +46,9 @@ export class CategoryRepository implements ICategoryRepository {
   }
 
   async findOneCategory(id: string) {
-    const oneCategory = await CategoryModel.findById(id);
-    return oneCategory;
+    const category = await CategoryModel.findById(id);
+    invariant(category !== null, new NotFoundError('id에 해당하는 카테고리가 존재하지 않습니다.'));
+    return category;
   }
 
   async createOneCategory(newCategory: createCategoryDto) {
