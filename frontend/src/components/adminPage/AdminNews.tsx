a4c161f1bf36cc1dd8a7366a61138519a10f59ed
SIYEA J <siyea0405@gmail.com>
2022-12-30 12:29:38 +0900

    fix: new Card 관련 기능수정

--- frontend/src/components/adminPage/AdminNews.tsx
+++ frontend/src/components/adminPage/AdminNews.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 
 import CreateNewsForm from './CreateNewsForm';
 import NewsCards from './NewsCards';
-
 import { useAdminCategoryStore } from '../../hooks/useAdminCategory';
 
 export default function AdminNews() {
-  const { adminCategory, adminCreateCardBtn, toggleAdminCreateCardBtn } = useAdminCategoryStore();
+  const { adminCreateCardBtn, toggleAdminCreateCardBtn } = useAdminCategoryStore();
   return (
     <>
       <div className="flex flex-col p-6 col-start-4 col-span-full row-span-full bg-garden2 lg:col-start-3">
         <div className="flex flex-row justify-between p-4 item-bottom">
-          <h2 className="flex text-4xl font-bold mb-5">{name} 관리</h2>
+          <h2 className="flex text-4xl font-bold mb-5">News 관리</h2>
         </div>
         <div className="flex flex-wrap">
-          <div className="flex flex-coltext-center m-3 p-3 w-full font-bold text-xl bg-white border border-gray-200 rounded-lg shadow-md dark:bg-[#292524] dark:border-[#292524]">
+          <div className="flex flex-col text-center m-3 p-3 w-full font-bold text-xl bg-white border border-gray-200 rounded-lg shadow-md dark:bg-[#292524] dark:border-[#292524]">
             <button className="w-full" onClick={() => toggleAdminCreateCardBtn()}>
               News 카드생성하기
             </button>
